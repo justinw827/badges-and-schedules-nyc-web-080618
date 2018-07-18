59a172041c2d4f782ed24d17c4ac0a7619b98de2
Justin Wang <justinw827@aol.com>
2018-07-18 20:16:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,14 @@ def batch_badge_creator(attendees)
   end
   return badges
 end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |speaker, i|
+    room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{i+1}")
+  end
+end
+
+def printer
+  
+end
